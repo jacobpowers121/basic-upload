54b0042a24914243dd77b13e098c7ab530271e1c
jacobpowers121 <jacob.powers121@gmail.com>
2024-10-17 18:55:23 -0500

    tidy up

--- basic-upload-service/src/main/java/com/example/basicuploadservice/controller/FileUploadController.java
+++ basic-upload-service/src/main/java/com/example/basicuploadservice/controller/FileUploadController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -18,6 +17,7 @@ public class FileUploadController {
 
     private final FileUploadService fileUploadService;
 
+    @Autowired
     public FileUploadController(FileUploadService fileUploadService) {
         this.fileUploadService = fileUploadService;
     }
